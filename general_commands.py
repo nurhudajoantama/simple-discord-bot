ad8520046ab0c4c9078436349e7bca5999808fbb
Nurhudaaa <nurhuda.joantama14@gmail.com>
2022-02-18 00:58:13 +0700

    add profile command n fix some style

--- general_commands.py
+++ general_commands.py
@@ -20,7 +20,23 @@ class GeneralCommands(commands.Cog, name='General commands'):
         emded = discord.Embed(
             title='Author',
             description=f'This bot is created by **{author.mention}**',
-            color=0x00ff00
+            color=0x003366
         )
-        emded.set_thumbnail(url=author.avatar_url)
+        emded.set_image(url=author.avatar_url)
+        await ctx.send(embed=emded)
+
+    @commands.command(name='profile', help='This command returns the profile of the user you mention')
+    async def profile(self, ctx):
+        user = ctx.message.mentions[0]
+        emded = discord.Embed(
+            title='Profile',
+            description=f'{user.mention}',
+            color=0x003366
+        )
+        emded.add_field(name='Display Name',
+                        value=user.display_name, inline=False)
+        emded.add_field(name='Name', value=user.name, inline=False)
+        emded.add_field(name='Status', value=user.status, inline=False)
+
+        emded.set_image(url=user.avatar_url)
         await ctx.send(embed=emded)
