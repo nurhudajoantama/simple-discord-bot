2c83359fcb04046eed3f4cd728596195d4a47b71
Nurhudaaa <nurhuda.joantama14@gmail.com>
2022-02-17 14:17:42 +0700

    true stream

--- music.py
+++ music.py
@@ -40,7 +40,7 @@ class YTDLSource(discord.PCMVolumeTransformer):
         self.url = data.get('url')
 
     @classmethod
-    async def from_url(cls, url, *, loop=None, stream=False):
+    async def from_url(cls, url, *, loop=None, stream=True):
         loop = loop or asyncio.get_event_loop()
         data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
 
