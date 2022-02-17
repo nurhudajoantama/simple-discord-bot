57a7e33a66da3631830debb9a634ba6e80854454
Nurhudaaa <nurhuda.joantama14@gmail.com>
2022-02-17 11:35:12 +0700

    add simple halo command

--- general_commands.py
+++ general_commands.py
@@ -9,3 +9,7 @@ class GeneralCommands(commands.Cog, name='General commands'):
     @commands.command(name='ping', help='This command returns the latency')
     async def ping(self, ctx):
         await ctx.send(f'**Pong!** Latency: {round(self.bot.latency * 1000)}ms')
+
+    @commands.command(name='halo', help='This command returns the Halo quote')
+    async def halo(self, ctx):
+        await ctx.send(f'_Hola_!!, {ctx.message.author.name}')
